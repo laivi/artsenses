b4db63c599464db6158222a5384c1a3b0efa891e
analais17 <analaismoraes17@gmail.com>
2019-10-18 21:48:54 -0300

    primeiro commit

---
+++ src/app/finale/finale.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-finale',
+  templateUrl: './finale.page.html',
+  styleUrls: ['./finale.page.scss'],
+})
+export class FinalePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
